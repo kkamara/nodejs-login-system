2437633f8ee6fc1d46ac0b57841cb85d6c9313ae
kkamara <kamaracomputers@gmail.com>
2024-11-12 23:00:02 +0000

    Connecting the database

--- app.js
+++ app.js
@@ -1,6 +1,33 @@
 const express = require("express");
+const mysql = require("mysql");
+const dotenv = require('dotenv');
+const path = require("path");
+
+const parseEnvFile = dotenv.config({
+    path: path.join(__dirname, '.env'),
+});
+
+if (parseEnvFile.error) {
+    throw parseEnvFile.error;
+}
 
 const app = express();
+
+const db = mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    port: process.env.MYSQL_PORT,
+});
+
+db.connect(error => {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log("MySQL connected.");
+    }
+});
 
 app.get("/", (req, res) => {
     res.send("<h1>Home Page</h1>");

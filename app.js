e7da8d779df171775f9db453f605f620b84a8573
kkamara <kamaracomputers@gmail.com>
2024-11-13 01:03:33 +0000

    Create APP_PORT environment variable

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ app.get("/", (req, res) => {
     res.send("<h1>Home Page</h1>");
 });
 
-app.listen(3000, () => {
-    console.log("Server started on port 3000.");
+const port = process.env.APP_PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server started on port ${port}.`);
 });

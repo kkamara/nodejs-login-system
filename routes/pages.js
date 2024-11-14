66141c281d3c9ed6232c83bfa8e12830853745ff
kkamara <kamaracomputers@gmail.com>
2024-11-14 20:50:39 +0000

    Add user profile page with hard-coded data

--- routes/pages.js
+++ routes/pages.js
@@ -14,4 +14,8 @@ router.get("/login", (req, res) => {
     return res.render("login");
 });
 
+router.get("/profile", (req, res) => {
+    return res.render("profile");
+});
+
 module.exports = router;
